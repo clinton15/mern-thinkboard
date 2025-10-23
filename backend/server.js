cfc5b77cef7e9a9c2d1e3a179d2754b9d4fc2afc
Clinton Pereira <clintonpereira@Clintons-MacBook-Air.local>
2025-10-23 15:58:08 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotnenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import router from "./routes/notesRoutes.js";
 import { connectToDb } from "./config/db.js";
@@ -23,19 +24,31 @@ theme in daisy ui
 //with a specific resource
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 dotnenv.config();
 
 //middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", router);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectToDb().then(() => {
   app.listen(PORT, () => {
